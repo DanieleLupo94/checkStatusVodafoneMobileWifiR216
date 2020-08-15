4dc69088c23721d1ca878026fa1c76a19a1ef8d4
Daniele Lupo <lupodaniele94@gmail.com>
2020-08-15 15:02:35 +0200

    - fix log visualizzabile in qualsiasi momento; - inserito il file run utilizzabile per il service.

--- main.py
+++ main.py
@@ -8,21 +8,20 @@ import dryscrape
 import webkit_server
 import os
 
-# TODO: Forse non serve creare una sessine ogni volta
-# FIXME: Il server di dryscape consuma risorse anche se il programma è in sleep
-# FIXME: Aumentare il tempo di attesa
+# TODO: Forse non serve creare una sessione ogni volta
 # TODO: Attraverso un campionamento uniforme, calcolare una stima di velocità di carica e scarica
-# FIXME: Se è in carica ed arriva al 100% non c'è più la gif ma è comunque in carica
 
 # In base all'analisi fatta, decido il rateo con cui si scarica e si carica la batteria
 rateoScaricamento = 1.5
 rateoCaricamento = 2
 
-audioAccendi = '/home/pi/Desktop/checkStatusVodafoneMobileWifiR216/accendi_caricabatteria.mp3'
+audioAccendi = './accendi_caricabatteria.mp3'
 
-audioSpegni = '/home/pi/Desktop/checkStatusVodafoneMobileWifiR216/spegni_caricabatteria.mp3'
+audioSpegni = './spegni_caricabatteria.mp3'
 
-pathFileLog = "/home/pi/Desktop/checkStatusVodafoneMobileWifiR216/log"
+attesaCaricamentoPagina = 30
+
+pathFileLog = "./log"
 
 url = 'http://192.168.0.1/html/home.htm'
 urlWebhook = 'https://maker.ifttt.com/trigger/CheckBatteria/with/key/crgmhm7kuG2plVg8e7W1_V'
@@ -34,19 +33,19 @@ server_conn = webkit_server.ServerConnection(server=server)
 driver = dryscrape.driver.webkit.Driver(connection=server_conn)
 session = dryscrape.Session(driver=driver)
 
-fileLog = open(pathFileLog,"w+")
+# fileLog = open(pathFileLog,"a+")
 
 inCarica = False
 
 def controllaStato():
-    print("[", time.asctime(time.localtime(time.time())), "] Visito la pagina ", url, file=fileLog)
+    salvaLog("Creo la sessione e visito la pagina " + url)
     session = dryscrape.Session()
     session.visit(url)
-    print("[", time.asctime(time.localtime(time.time())), "] Attendo che venga caricata la pagina ", url, file=fileLog)
-    time.sleep(15)  # Attendo che venga caricata la pagina
+    salvaLog("Attendo che venga caricata la pagina " + url)
+    time.sleep(attesaCaricamentoPagina)  # Attendo che venga caricata la pagina
     response = session.body()
     session.reset()  # Per evitare il memory leak
-    print("[", time.asctime(time.localtime(time.time())), "] Ho visitato la pagina", file=fileLog)
+    salvaLog("Ho visitato la pagina " + url)
 
     soup = BeautifulSoup(response, 'html.parser')
     # FIXME: Se la pagina non è caricata completamente, il div è vuoto. Aggiungere questo controllo
@@ -59,7 +58,7 @@ def controllaStato():
     stato = stato[2]
     # Tolgo le parentesi ed il simbolo della percentuale
     stato = stato[1:-2]
-    print("[", time.asctime(time.localtime(time.time())), "] Batteria: ", stato, file=fileLog)
+    salvaLog("Batteria: " + stato)
     stato = int(stato)
 
     # Se è in carica viene mostrata la gif di caricamento
@@ -67,46 +66,61 @@ def controllaStato():
     imgBatteria = imgBatteria['src'].rsplit('/')[2]
     imgBatteria = imgBatteria.rsplit('?')[0]
     inCarica = (imgBatteria == 'batteryCharging.gif')
-    print("[", time.asctime(time.localtime(time.time())), "] In carica: ",  inCarica, file=fileLog)
+    if (inCarica):
+        inCarica = "true"
+    else:
+        inCarica = "false"
+    salvaLog("In carica: " + inCarica)
 
     if inCarica and stato != 100:
-        print("[", time.asctime(time.localtime(time.time())), "] Attendo ", (100 - stato) * rateoCaricamento, " minuti", file=fileLog)
-        time.sleep(rateoCaricamento * 60 * (100-stato))  # Attesa dinamica
+        tempoAttesa = rateoCaricamento * (100-stato)
+        salvaLog("Attendo " + str(tempoAttesa) + " minuti")
+        tempoAttesa = tempoAttesa * 60
+        time.sleep(tempoAttesa)  # Attesa dinamica
         controllaStato()
     else:
         # TODO: Pensare ad un modo dinamico di calcolare il tempo di attesa in base alla velocità di scaricamento della batteria
         if stato < 11:
-            print("[", time.asctime(time.localtime(time.time())), "] Batteria scarica. Invio la notifica ed attendo 5 minuti.", file=fileLog)
-            # TODO: Far accendere la presa da Alexa
-            # Chiamo il webhook per mandare la notifica
-            # session.visit(urlWebhook)
-	    # Riproduco il file audio in modo che Alexa senta il comando
+            salvaLog("Batteria scarica. Invio la notifica ed attendo 5 minuti.")
+			# Riproduco il file audio in modo che Alexa senta il comando
             os.system("omxplayer " + audioAccendi)
+            # Chiamo il webhook per mandare la notifica
             requests.post(urlWebhook, json={
                           'value1': 'Batteria scarica. Accendere la presa'})
             time.sleep(60*5)  # Attesa di 5 minuti
             controllaStato()
         else:
             if stato == 100:
-                # TODO: Spegnere il caricabatteria
+				# Chiamo il webhook per mandare la notifica
                 requests.post(urlWebhook, json={
                               'value1': 'Batteria carica. Spegnere la presa'})
-		# Riproduco il comando in modo che Alexa possa sentirlo
+				# Riproduco il comando in modo che Alexa possa sentirlo
                 os.system("omxplayer " + audioSpegni)
             # Ho calcolato in questo modo l'attesa in modo da avere un'attesa massima quando è carica e poi decrescente insieme alla batteria
             attesa = 100 - (100 - stato)
             attesa = attesa * rateoScaricamento
-            print("[", time.asctime(time.localtime(time.time())),
-                  "] Attendo ", attesa, " minuti", file=fileLog)
+            attesa = attesa
+            salvaLog("Attendo " + str(attesa) + " minuti")
             # Attesa dinamica "inversa"
-            time.sleep(60 * attesa)
+            attesa = attesa * 60
+            time.sleep(attesa)
             controllaStato()
 
+
+# Salvo il log nel file e lo chiudo subito
+def salvaLog(testo):
+	fileLog = open(pathFileLog,"a+")
+	# Aggiungo il timestamp al log
+	t = "[" + time.asctime(time.localtime(time.time())) + "] " + str(testo)
+	fileLog.write(t)
+	fileLog.write("\n")
+	# print(">> ", t)
+	fileLog.close()
 
 try:
     controllaStato()
 finally:
-    print("[", time.asctime(time.localtime(time.time())), "] Killo il server", file=fileLog)
+    salvaLog("Killo il server.")
+    # fileLog.close()
     server.kill()  # Altrimenti resta attivo
-    fileLog.close()
 
